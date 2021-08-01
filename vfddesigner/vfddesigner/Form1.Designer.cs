5cd2a125ea9ebcf2c9a351f211fa4b3ad738824f
Nathan <nsafran1217@gmail.com>
2021-07-31 23:21:28 -0400

    added designer, EAGLE files, test arduino sketch

---
+++ vfddesigner/vfddesigner/Form1.Designer.cs
@@ -0,0 +1,368 @@
+﻿namespace vfddesigner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.segbut_1 = new System.Windows.Forms.Button();
+            this.segbut_2 = new System.Windows.Forms.Button();
+            this.segbut_3 = new System.Windows.Forms.Button();
+            this.segbut_4 = new System.Windows.Forms.Button();
+            this.segbut_5 = new System.Windows.Forms.Button();
+            this.segbut_6 = new System.Windows.Forms.Button();
+            this.segbut_7 = new System.Windows.Forms.Button();
+            this.segbut_8 = new System.Windows.Forms.Button();
+            this.segbut_9 = new System.Windows.Forms.Button();
+            this.segbut_10 = new System.Windows.Forms.Button();
+            this.segbut_11 = new System.Windows.Forms.Button();
+            this.segbut_12 = new System.Windows.Forms.Button();
+            this.segbut_13 = new System.Windows.Forms.Button();
+            this.segbut_14 = new System.Windows.Forms.Button();
+            this.segbut_15 = new System.Windows.Forms.Button();
+            this.segbut_16 = new System.Windows.Forms.Button();
+            this.outTextBox = new System.Windows.Forms.TextBox();
+            this.segbut_17 = new System.Windows.Forms.Button();
+            this.segbut_18 = new System.Windows.Forms.Button();
+            this.charTextBox = new System.Windows.Forms.TextBox();
+            this.nextbtn = new System.Windows.Forms.Button();
+            this.outLines = new System.Windows.Forms.TextBox();
+            this.asciiTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // segbut_1
+            // 
+            this.segbut_1.Location = new System.Drawing.Point(185, 104);
+            this.segbut_1.Name = "segbut_1";
+            this.segbut_1.Size = new System.Drawing.Size(75, 23);
+            this.segbut_1.TabIndex = 0;
+            this.segbut_1.Text = "a";
+            this.segbut_1.UseVisualStyleBackColor = true;
+            this.segbut_1.Click += new System.EventHandler(this.segbut_a_Click);
+            // 
+            // segbut_2
+            // 
+            this.segbut_2.Location = new System.Drawing.Point(275, 105);
+            this.segbut_2.Name = "segbut_2";
+            this.segbut_2.Size = new System.Drawing.Size(75, 23);
+            this.segbut_2.TabIndex = 1;
+            this.segbut_2.Text = "b";
+            this.segbut_2.UseVisualStyleBackColor = true;
+            this.segbut_2.Click += new System.EventHandler(this.segbut_b_Click);
+            // 
+            // segbut_3
+            // 
+            this.segbut_3.Location = new System.Drawing.Point(347, 123);
+            this.segbut_3.Name = "segbut_3";
+            this.segbut_3.Size = new System.Drawing.Size(23, 108);
+            this.segbut_3.TabIndex = 2;
+            this.segbut_3.Text = "c";
+            this.segbut_3.UseVisualStyleBackColor = true;
+            this.segbut_3.Click += new System.EventHandler(this.segbut_c_Click);
+            // 
+            // segbut_4
+            // 
+            this.segbut_4.Location = new System.Drawing.Point(347, 237);
+            this.segbut_4.Name = "segbut_4";
+            this.segbut_4.Size = new System.Drawing.Size(23, 108);
+            this.segbut_4.TabIndex = 3;
+            this.segbut_4.Text = "d";
+            this.segbut_4.UseVisualStyleBackColor = true;
+            this.segbut_4.Click += new System.EventHandler(this.segbut_d_Click);
+            // 
+            // segbut_5
+            // 
+            this.segbut_5.Location = new System.Drawing.Point(275, 351);
+            this.segbut_5.Name = "segbut_5";
+            this.segbut_5.Size = new System.Drawing.Size(75, 23);
+            this.segbut_5.TabIndex = 4;
+            this.segbut_5.Text = "e";
+            this.segbut_5.UseVisualStyleBackColor = true;
+            this.segbut_5.Click += new System.EventHandler(this.segbut_e_Click);
+            // 
+            // segbut_6
+            // 
+            this.segbut_6.Location = new System.Drawing.Point(185, 351);
+            this.segbut_6.Name = "segbut_6";
+            this.segbut_6.Size = new System.Drawing.Size(75, 23);
+            this.segbut_6.TabIndex = 5;
+            this.segbut_6.Text = "f";
+            this.segbut_6.UseVisualStyleBackColor = true;
+            this.segbut_6.Click += new System.EventHandler(this.segbut_f_Click);
+            // 
+            // segbut_7
+            // 
+            this.segbut_7.Location = new System.Drawing.Point(165, 237);
+            this.segbut_7.Name = "segbut_7";
+            this.segbut_7.Size = new System.Drawing.Size(23, 108);
+            this.segbut_7.TabIndex = 6;
+            this.segbut_7.Text = "g";
+            this.segbut_7.UseVisualStyleBackColor = true;
+            this.segbut_7.Click += new System.EventHandler(this.segbut_g_Click);
+            // 
+            // segbut_8
+            // 
+            this.segbut_8.Location = new System.Drawing.Point(165, 123);
+            this.segbut_8.Name = "segbut_8";
+            this.segbut_8.Size = new System.Drawing.Size(23, 108);
+            this.segbut_8.TabIndex = 7;
+            this.segbut_8.Text = "h";
+            this.segbut_8.UseVisualStyleBackColor = true;
+            this.segbut_8.Click += new System.EventHandler(this.segbut_h_Click);
+            // 
+            // segbut_9
+            // 
+            this.segbut_9.Location = new System.Drawing.Point(207, 152);
+            this.segbut_9.Name = "segbut_9";
+            this.segbut_9.Size = new System.Drawing.Size(44, 48);
+            this.segbut_9.TabIndex = 8;
+            this.segbut_9.Text = "i";
+            this.segbut_9.UseVisualStyleBackColor = true;
+            this.segbut_9.Click += new System.EventHandler(this.segbut_i_Click);
+            // 
+            // segbut_10
+            // 
+            this.segbut_10.Location = new System.Drawing.Point(257, 123);
+            this.segbut_10.Name = "segbut_10";
+            this.segbut_10.Size = new System.Drawing.Size(23, 108);
+            this.segbut_10.TabIndex = 9;
+            this.segbut_10.Text = "j";
+            this.segbut_10.UseVisualStyleBackColor = true;
+            this.segbut_10.Click += new System.EventHandler(this.segbut_j_Click);
+            // 
+            // segbut_11
+            // 
+            this.segbut_11.Location = new System.Drawing.Point(286, 152);
+            this.segbut_11.Name = "segbut_11";
+            this.segbut_11.Size = new System.Drawing.Size(44, 48);
+            this.segbut_11.TabIndex = 10;
+            this.segbut_11.Text = "k";
+            this.segbut_11.UseVisualStyleBackColor = true;
+            this.segbut_11.Click += new System.EventHandler(this.segbut_k_Click);
+            // 
+            // segbut_12
+            // 
+            this.segbut_12.Location = new System.Drawing.Point(275, 225);
+            this.segbut_12.Name = "segbut_12";
+            this.segbut_12.Size = new System.Drawing.Size(75, 23);
+            this.segbut_12.TabIndex = 11;
+            this.segbut_12.Text = "l";
+            this.segbut_12.UseVisualStyleBackColor = true;
+            this.segbut_12.Click += new System.EventHandler(this.segbut_l_Click);
+            // 
+            // segbut_13
+            // 
+            this.segbut_13.Location = new System.Drawing.Point(286, 266);
+            this.segbut_13.Name = "segbut_13";
+            this.segbut_13.Size = new System.Drawing.Size(42, 50);
+            this.segbut_13.TabIndex = 12;
+            this.segbut_13.Text = "m";
+            this.segbut_13.UseVisualStyleBackColor = true;
+            this.segbut_13.Click += new System.EventHandler(this.segbut_m_Click);
+            // 
+            // segbut_14
+            // 
+            this.segbut_14.Location = new System.Drawing.Point(257, 237);
+            this.segbut_14.Name = "segbut_14";
+            this.segbut_14.Size = new System.Drawing.Size(23, 108);
+            this.segbut_14.TabIndex = 13;
+            this.segbut_14.Text = "n";
+            this.segbut_14.UseVisualStyleBackColor = true;
+            this.segbut_14.Click += new System.EventHandler(this.segbut_n_Click);
+            // 
+            // segbut_15
+            // 
+            this.segbut_15.Location = new System.Drawing.Point(207, 266);
+            this.segbut_15.Name = "segbut_15";
+            this.segbut_15.Size = new System.Drawing.Size(44, 51);
+            this.segbut_15.TabIndex = 14;
+            this.segbut_15.Text = "o";
+            this.segbut_15.UseVisualStyleBackColor = true;
+            this.segbut_15.Click += new System.EventHandler(this.segbut_o_Click);
+            // 
+            // segbut_16
+            // 
+            this.segbut_16.Location = new System.Drawing.Point(185, 225);
+            this.segbut_16.Name = "segbut_16";
+            this.segbut_16.Size = new System.Drawing.Size(75, 23);
+            this.segbut_16.TabIndex = 15;
+            this.segbut_16.Text = "p";
+            this.segbut_16.UseVisualStyleBackColor = true;
+            this.segbut_16.Click += new System.EventHandler(this.segbut_p_Click);
+            // 
+            // outTextBox
+            // 
+            this.outTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outTextBox.Location = new System.Drawing.Point(411, 25);
+            this.outTextBox.Name = "outTextBox";
+            this.outTextBox.ReadOnly = true;
+            this.outTextBox.Size = new System.Drawing.Size(343, 31);
+            this.outTextBox.TabIndex = 16;
+            // 
+            // segbut_17
+            // 
+            this.segbut_17.Location = new System.Drawing.Point(136, 351);
+            this.segbut_17.Name = "segbut_17";
+            this.segbut_17.Size = new System.Drawing.Size(30, 50);
+            this.segbut_17.TabIndex = 17;
+            this.segbut_17.Text = "17";
+            this.segbut_17.UseVisualStyleBackColor = true;
+            this.segbut_17.Click += new System.EventHandler(this.segbut_17_Click);
+            // 
+            // segbut_18
+            // 
+            this.segbut_18.Location = new System.Drawing.Point(366, 350);
+            this.segbut_18.Name = "segbut_18";
+            this.segbut_18.Size = new System.Drawing.Size(28, 51);
+            this.segbut_18.TabIndex = 18;
+            this.segbut_18.Text = "18";
+            this.segbut_18.UseVisualStyleBackColor = true;
+            this.segbut_18.Click += new System.EventHandler(this.segbut_18_Click);
+            // 
+            // charTextBox
+            // 
+            this.charTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.charTextBox.Location = new System.Drawing.Point(432, 152);
+            this.charTextBox.Name = "charTextBox";
+            this.charTextBox.ReadOnly = true;
+            this.charTextBox.Size = new System.Drawing.Size(115, 31);
+            this.charTextBox.TabIndex = 19;
+            // 
+            // nextbtn
+            // 
+            this.nextbtn.Location = new System.Drawing.Point(490, 80);
+            this.nextbtn.Name = "nextbtn";
+            this.nextbtn.Size = new System.Drawing.Size(161, 48);
+            this.nextbtn.TabIndex = 20;
+            this.nextbtn.Text = "nextchar";
+            this.nextbtn.UseVisualStyleBackColor = true;
+            this.nextbtn.Click += new System.EventHandler(this.nextbtn_Click);
+            // 
+            // outLines
+            // 
+            this.outLines.Location = new System.Drawing.Point(432, 210);
+            this.outLines.Multiline = true;
+            this.outLines.Name = "outLines";
+            this.outLines.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.outLines.Size = new System.Drawing.Size(356, 228);
+            this.outLines.TabIndex = 21;
+            this.outLines.WordWrap = false;
+            // 
+            // asciiTextBox
+            // 
+            this.asciiTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.asciiTextBox.Location = new System.Drawing.Point(553, 152);
+            this.asciiTextBox.Name = "asciiTextBox";
+            this.asciiTextBox.Size = new System.Drawing.Size(100, 31);
+            this.asciiTextBox.TabIndex = 22;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(432, 133);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "Char";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(553, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "ascii (dec)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.asciiTextBox);
+            this.Controls.Add(this.outLines);
+            this.Controls.Add(this.nextbtn);
+            this.Controls.Add(this.charTextBox);
+            this.Controls.Add(this.segbut_18);
+            this.Controls.Add(this.segbut_17);
+            this.Controls.Add(this.outTextBox);
+            this.Controls.Add(this.segbut_16);
+            this.Controls.Add(this.segbut_15);
+            this.Controls.Add(this.segbut_14);
+            this.Controls.Add(this.segbut_13);
+            this.Controls.Add(this.segbut_12);
+            this.Controls.Add(this.segbut_11);
+            this.Controls.Add(this.segbut_10);
+            this.Controls.Add(this.segbut_9);
+            this.Controls.Add(this.segbut_8);
+            this.Controls.Add(this.segbut_7);
+            this.Controls.Add(this.segbut_6);
+            this.Controls.Add(this.segbut_5);
+            this.Controls.Add(this.segbut_4);
+            this.Controls.Add(this.segbut_3);
+            this.Controls.Add(this.segbut_2);
+            this.Controls.Add(this.segbut_1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button segbut_1;
+        private System.Windows.Forms.Button segbut_2;
+        private System.Windows.Forms.Button segbut_3;
+        private System.Windows.Forms.Button segbut_4;
+        private System.Windows.Forms.Button segbut_5;
+        private System.Windows.Forms.Button segbut_6;
+        private System.Windows.Forms.Button segbut_7;
+        private System.Windows.Forms.Button segbut_8;
+        private System.Windows.Forms.Button segbut_9;
+        private System.Windows.Forms.Button segbut_10;
+        private System.Windows.Forms.Button segbut_11;
+        private System.Windows.Forms.Button segbut_12;
+        private System.Windows.Forms.Button segbut_13;
+        private System.Windows.Forms.Button segbut_14;
+        private System.Windows.Forms.Button segbut_15;
+        private System.Windows.Forms.Button segbut_16;
+        private System.Windows.Forms.TextBox outTextBox;
+        private System.Windows.Forms.Button segbut_17;
+        private System.Windows.Forms.Button segbut_18;
+        private System.Windows.Forms.TextBox charTextBox;
+        private System.Windows.Forms.Button nextbtn;
+        private System.Windows.Forms.TextBox outLines;
+        private System.Windows.Forms.TextBox asciiTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ vfddesigner/vfddesigner/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vfddesigner
+{
+    public partial class Form1 : Form
+    {
+
+        UInt32 thirtytwobitone = 1;
+        UInt32 code = 0;
+        public int asciicode = 48;
+        public Form1()
+        {
+            InitializeComponent();
+            updateCode();
+        }
+
+        public void updateCode()
+        {
+            String binary = (Convert.ToString(code, 2)).PadLeft(20,'0');
+
+            this.outTextBox.Text = "0b" +(binary) + ",  //" + Convert.ToChar(asciicode).ToString();          
+            this.charTextBox.Text = Convert.ToChar(asciicode).ToString();
+            this.asciiTextBox.Text = Convert.ToString(asciicode);
+ 
+        }
+
+        private void nextbtn_Click(object sender, EventArgs e)
+        {
+            asciicode = Convert.ToInt16(asciiTextBox.Text);
+            String binary = (Convert.ToString(code, 2)).PadLeft(20, '0');
+            this.outLines.AppendText("0b" + (binary) + ",  //" + Convert.ToChar(asciicode).ToString() + "\r\n");
+            asciicode++; //inc ascii code to next CHAR
+            updateCode();
+
+        }
+        private void toggleColor(object sent, int index)
+        {
+            Button clickedButton = (Button)sent;
+            if (clickedButton.BackColor.Equals(Color.Yellow))
+            {
+                clickedButton.BackColor = Color.Transparent;
+                code = code ^ (thirtytwobitone << index); //xor to get rid of bit
+            }
+            else
+            {
+                clickedButton.BackColor = Color.Yellow;
+                code = code | (thirtytwobitone << index);   //or to get bit
+            }
+
+            updateCode();
+        }
+
+        private void segbut_a_Click(object sender, EventArgs e)
+        {
+            toggleColor(sender,0);
+
+        }
+
+        private void segbut_b_Click(object sender, EventArgs e)
+        {
+            toggleColor(sender,1);
+        }
+
+        private void segbut_c_Click(object sender, EventArgs e)
+        {
+            toggleColor(sender,2);
+        }
+
+        private void segbut_d_Click(object sender, EventArgs e)
+        {
+            toggleColor(sender,3);
+        }
+
+        private void segbut_e_Click(object sender, EventArgs e)
+        {
+            toggleColor(sender,4);
+        }
+
+        private void segbut_f_Click(object sender, EventArgs e)
+        {
+            toggleColor(sender,5);
+        }
+
+        private void segbut_g_Click(object sender, EventArgs e)
+        {
+            toggleColor(sender,6);
+        }
+
+        private void segbut_h_Click(object sender, EventArgs e)
+        {
+            toggleColor(sender,7);
+        }
+
+        private void segbut_i_Click(object sender, EventArgs e)
+        {
+            toggleColor(sender,8);
+        }
+
+        private void segbut_j_Click(object sender, EventArgs e)
+        {
+            toggleColor(sender,9);
+        }
+
+        private void segbut_k_Click(object sender, EventArgs e)
+        {
+            toggleColor(sender,10);
+        }
+
+        private void segbut_l_Click(object sender, EventArgs e)
+        {
+            toggleColor(sender,11);
+        }
+
+        private void segbut_m_Click(object sender, EventArgs e)
+        {
+            toggleColor(sender,12);
+        }
+
+        private void segbut_n_Click(object sender, EventArgs e)
+        {
+            toggleColor(sender,13);
+        }
+
+        private void segbut_o_Click(object sender, EventArgs e)
+        {
+            toggleColor(sender,14);
+        }
+
+        private void segbut_p_Click(object sender, EventArgs e)
+        {
+            toggleColor(sender,15);
+        }
+        private void segbut_17_Click(object sender, EventArgs e)
+        {
+            toggleColor(sender,16);
+        }
+
+        private void segbut_18_Click(object sender, EventArgs e)
+        {
+            toggleColor(sender,17);
+        }
+
+    }
+}
